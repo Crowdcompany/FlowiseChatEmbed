f9dcba67aa31230273d4bce2fb7575f7070d5f9a
Crowdcompany <raimund.bauer@crowdcompany-ug.com>
2024-03-03 18:23:48 +0100

    18:23

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -965,9 +965,8 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
               />
             )}
           </div>
-          
-          {/*<Badge badgeBackgroundColor={props.badgeBackgroundColor} poweredByTextColor={props.poweredByTextColor} botContainer={botContainer} />*/}
-          
+
+          _{/*<Badge badgeBackgroundColor={props.badgeBackgroundColor} poweredByTextColor={props.poweredByTextColor} botContainer={botContainer} />*/}_
         </div>
       </div>
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
